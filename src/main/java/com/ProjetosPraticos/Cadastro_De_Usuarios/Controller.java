6701aff8ad8959167c64e2a475c21000b9413fa5
Gabriel Brasil <gabrielbrasil87@outlook.com>
2025-07-07 15:07:44 -0300

    atualização

--- src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Controller.java
+++ src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Controller.java
@@ -1,4 +1,18 @@
 package com.ProjetosPraticos.Cadastro_De_Usuarios;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Olá, Mundo!";
+    }
+
+
 }
