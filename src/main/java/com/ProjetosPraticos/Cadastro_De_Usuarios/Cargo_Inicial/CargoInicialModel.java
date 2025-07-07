201061d46fd2c585da272b3b5f2830da6ec4a329
Gabriel Brasil <gabrielbrasil87@outlook.com>
2025-07-07 19:14:55 -0300

    atualizando pool

--- src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Cargo_Inicial/CargoInicialModel.java
+++ src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Cargo_Inicial/CargoInicialModel.java
@@ -2,10 +2,16 @@ package com.ProjetosPraticos.Cadastro_De_Usuarios.Cargo_Inicial;
 
 import com.ProjetosPraticos.Cadastro_De_Usuarios.Funcionarios.FuncionarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Entity
 @Table(name="tb_cargos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CargoInicialModel {
 
     @Id
--- src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Funcionarios/FuncionarioModel.java
+++ src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/Funcionarios/FuncionarioModel.java
@@ -2,10 +2,16 @@ package com.ProjetosPraticos.Cadastro_De_Usuarios.Funcionarios;
 
 import com.ProjetosPraticos.Cadastro_De_Usuarios.Cargo_Inicial.CargoInicialModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //transforma classe em entidade no banco de dados
 @Entity
 @Table (name = "tb_funcionario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FuncionarioModel {
 
     @Id
@@ -14,41 +20,8 @@ public class FuncionarioModel {
     private String nome;
     private String email;
     private int idade;
+
     @ManyToOne
     @JoinColumn(name = "cargos_id")
-
     private CargoInicialModel cargoinicial;
-
-    public FuncionarioModel() {
-    }
-
-    public FuncionarioModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
