080eda6b6150057986658d0f5c39e19ad191b8bc
Gabriel Brasil <gabrielbrasil87@outlook.com>
2025-07-07 13:46:30 -0300

    first commit

---
+++ src/main/java/com/ProjetosPraticos/Cadastro_De_Usuarios/CadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetosPraticos.Cadastro_De_Usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjetosPraticos/Cadastro_De_Usuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetosPraticos.Cadastro_De_Usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
